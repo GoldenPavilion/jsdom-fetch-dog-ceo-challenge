76f82ec40c7ec28655652172c66285cbddf662fa
GoldenPavilion <rcorystine@gmail.com>
2021-07-28 17:16:57 -0400

    code inserted but still doesn't display

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const container = document.querySelector("#dog-image-container");
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
 fetch(imgUrl).then(function(response){
     return response.json();
 }).then (images => {
-    debugger;
+    const imgs = images.message;
+    let imgsArray = imgs.map((image) => {
+        let i = `<img scr=${image}>`;
+        return i;
+    })
+    imgsArray.forEach(element =>{
+        container.innerHTML += element
+    }); 
 })
