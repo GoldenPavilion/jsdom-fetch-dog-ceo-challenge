b9aa1d98f2d4f9d6c897d91183d02099d0ea159f
GoldenPavilion <rcorystine@gmail.com>
2021-07-28 16:25:01 -0400

    shifted script tag

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl).then(function(response){
+    return response.json();
+}).then (function(json){
+    console.log(imgUrl);
+})
